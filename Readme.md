dbcaf9a4495218d4cef8370b4fa8e4634b136925
MichaelLaw2003 <1983313749@qq.com>
2025-03-21 20:55:14 +0800

    Conducted a retrospective review on what I've done last year.

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Readme
+
+2025 年 1 月 6 日
+
+.drawio 文件是用来画流程图的，别删，用 draw.io 可以打开
+
+2025 年 3 月 21 日
+
+主页是 index.html，该可视化程序用 live-server 可以正常运行。
--- Template/chart.js
+++ Template/chart.js
@@ -1,5 +1,5 @@
 //console.log(d3);
-
+//这个js文件用于可视化基因序列和注释
 let w = window.innerWidth;
 let h = window.innerHeight;
 const dms = {
@@ -34,13 +34,11 @@ const content = box.append('g')
 //The lines above can be reused.
 const axis = content.append('g')
     .attr('id', 'axis');
-//.style('transform',`translateY(${dms.contentHeight/2}px)`);
 
 const featureArea = content.append('g')
     .attr('id', 'feature-area');
-//.style('transform',`translateY(${dms.contentHeight/2}px)`);
 
-d3.json('../data/human_mito.json')
+d3.json('../data/human_insulin.json') //.json文件地址
     .then(drawLinear);
 
 function drawLinear(dataset) {
@@ -58,7 +56,6 @@ function drawLinear(dataset) {
     const informationGet = d => d.information;
 
     const totalLength = locusGet(dataset)[1];
-    //console.log(totalLength);
 
     const LengthScale = d3.scaleLinear()
         .domain([0, totalLength])
@@ -73,35 +70,39 @@ function drawLinear(dataset) {
         .style("font-size", dms.fontSize);
 
     var row = 1;
-    var i = 1;//i is used to attribute the id of the features. IMORTANT!
+    i = 1;//i is used to attribute the id of the features. IMORTANT!
     var intervals = new Array();
     var occupied = {};
-    intervals[0] = [0,0];
+    intervals[0] = [0, 0];
     occupied[row] = intervals;
-    //console.log(`intervals:${intervals[1][1]}`);
 
     var start = 0;
     var end = 0;
 
-    function display(g, type, location, r, displayBox='true',displayText='true', text=''){//g for group; r for row;
-        let cleanedStr = str=>str.replace('>', '').replace('<', '');
+    function display(type, location, r, displayBox = true, displayText = true, text = '') {//g for group; r for row;
+        const featureDisplay = featureArea.append('g')
+            .attr('id', 'id', `${"feature" + i}`)
+            .attr('class', "feature");
+        //Create a group for each feature.
+        let cleanedStr = str => str.replace('>', '').replace('<', '');
         var startStr = location[0][0];
         var endStr = location[location.length - 1].at(-1);
         var start = Number(cleanedStr(startStr));
         var end = Number(cleanedStr(endStr));
-        if (end == start){
+        /*
+        if (end == start) {
             end = start + 2;
-            start = start -2;
+            start = start - 2;
         }
-        
-        if (displayBox){
+        */
+        if (displayBox) {
             j = 1;
-            const boxGroup = g.append('g')
-                .attr('id', `${"feature-box"}${i}`)
+            const boxGroup = featureDisplay.append('g')
+                .attr('id', `${"feature-box-group"}-${i}`)
                 .attr('class', `box-group`);
-            
+
             boxGroup.append('line')
-                .attr('id', `${"feature-bone"}${i}`)
+                .attr('id', `${"feature-bone"}-${i}`)
                 .attr('class', `box bone ${type}`)
                 .attr('x1', LengthScale(start))
                 .attr('y1', 0)
@@ -109,141 +110,208 @@ function drawLinear(dataset) {
                 .attr('y2', 0)
                 .style('transform', `translateY(${dms.vSpace * r}px)`);
 
-            location.forEach(loc=>{
+            var locationArray = new Array();
+
+            location.forEach(loc => {
                 var BoxStartStr = loc[0];
                 var BoxEndStr = loc.at(-1);
                 var BoxStart = Number(cleanedStr(BoxStartStr));
                 var BoxEnd = Number(cleanedStr(BoxEndStr));
+                locationArray.push(BoxStart, BoxEnd);
                 boxGroup.append('line')
-                .attr('id', `${"feature-box"}${i}-${j}`)
-                .attr('class', `box ${type}`)
-                .attr('x1', LengthScale(BoxStart))
-                .attr('y1', 0)
-                .attr('x2', LengthScale(BoxEnd))
-                .attr('y2', 0)
-                .style('transform', `translateY(${dms.vSpace * r}px)`);
+                    .attr('id', `${"feature-box"}${i}-${j}`)
+                    .attr('class', `box ${type}`)
+                    .attr('x1', LengthScale(BoxStart))
+                    .attr('y1', 0)
+                    .attr('x2', LengthScale(BoxEnd))
+                    .attr('y2', 0)
+                    .style('transform', `translateY(${dms.vSpace * r}px)`);
                 j++;
             })
         }
-        if (displayText){
-            g.append('text')
-            .attr('id', `${"feature-annotation" + i}`)
-            .attr('class', 'annotation')
-            .attr('x', `${LengthScale((start + end) / 2)}`)
-            .attr('y', `${dms.vSpace * r + dms.unit * 0.5}`)
-            .text(text)
-            trimText(g, i);
+
+        if (displayText) {
+            featureDisplay.append('text')
+                .attr('id', `feature-annotation-${i}`)
+                .attr('class', 'annotation')
+                .attr('x', `${LengthScale((start + end) / 2)}`)
+                .attr('y', `${dms.vSpace * r + dms.unit * 0.5}`)
+                .text(text);
+
+            var trimmedText = trimText(i);
+            featureDisplay.select(`#feature-annotation-${i}`)
+                .text(trimmedText);
+
         }
-    i++;
+
+        var annotation = document.querySelector(`#feature-annotation-${i}`);
+
+        featureDisplay.on("mouseover", function () {
+            if (annotation) {
+                const textWidth = annotation.getBBox().width;
+                d3.select(this).raise();
+                d3.select(this).append('line')//Background of annotation text.
+                    .attr('id', `annotation-bg-${i}`)
+                    .attr('class', `annotationg-bg`)
+                    .attr('x1', `${LengthScale((start + end) / 2) - textWidth*1.5 / 2}`)
+                    .attr('y1', 0)
+                    .attr('x2', `${LengthScale((start + end) / 2) + textWidth*1.5 / 2}`)
+                    .attr('y2', 0)
+                    .style('transform', `translateY(${dms.vSpace * r}px)`)
+                    .style('stroke-linecap', 'round')
+                    .style('stroke-width', dms.boxHeight);
+
+                d3.select(this).select('.annotation')//Display FULL annotation text.
+                    .text(text)
+                    .raise();
+            }
+
+            locationArray.forEach(d => {
+                const locationVis = d3.select(this).append('g')
+                    .attr('class', 'location');
+
+                locationVis.append('line')
+                    .attr('class', 'location-mark')
+                    .attr('x1', LengthScale(d))
+                    .attr('y1', 0)
+                    .attr('x2', LengthScale(d))
+                    .attr('y2', 6);
+                
+                const formatNum = d3.format(",");
+
+                locationVis.append('text')
+                    .attr('class', 'location-text')
+                    .attr('x', LengthScale(d))
+                    .attr('y', 18)
+                    .text(formatNum(d));        
+            })
+        })
+
+        featureDisplay.on("mouseout", function () {
+            d3.select(this).lower();
+            d3.select(this).select('.annotation').text(trimmedText);
+            d3.select(this).select(`#annotation-bg-${i}`)
+                .remove();
+            d3.select(this).selectAll(`.location`).remove();
+        })
+
+        i++;
     }
 
     feature.forEach(d => {
         var row = 1;
         const location = locationGet(d);
-        let cleanedStr = str=>str.replace('>', '').replace('<', '');
+        let cleanedStr = str => str.replace('>', '').replace('<', '');
         var startStr = location[0][0];
         var endStr = location[location.length - 1].at(-1);
         var start = Number(cleanedStr(startStr));
         var end = Number(cleanedStr(endStr));
         interval = [start, end];
-        //console.log(`type: ${typeGet(d)}, location: ${interval}`);
         while (checkOccupation(row, interval, occupied)) {
             row++;
         }
         occupied[row].push(interval);
-
-        const featureDisplay = featureArea.append('g')
-            .attr('id', 'id', `${"feature" + i}`)
-            .attr('class', "feature"); //Create a group for each feature.
-
         if (typeGet(d) == 'source') {
-            display(g=featureDisplay, type='source', location, r=row, displayBox='true',displayText='false');
+            display(type = 'source', location, r = row, displayBox = false, displayText = false);
         }//Display source.
         else if (typeGet(d) == 'operon') {
             var annotationText = `${informationGet(d).operon} (operon)`;
-            display(g=featureDisplay, type='operon', location, r=row, displayBox='true',displayText='true', text=annotationText);
+            display(type = 'operon', location, r = row, displayBox = true, displayText = true, text = annotationText);
         }//Display operon.
         else if (typeGet(d) == 'CDS') {
             var annotationText = `${informationGet(d).product}`;
-            display(g=featureDisplay, type='CDS', location, r=row, displayBox='true',displayText='true', text=annotationText);
+            display(type = 'CDS', location, r = row, displayBox = true, displayText = true, text = annotationText);
         }//Display CDS.
         else if (typeGet(d) == 'gene') {
             var annotationText = `${informationGet(d).gene}`;
-            display(g=featureDisplay, type='gene', location, r=row, displayBox='true',displayText='true', text=annotationText);
+            display(type = 'gene', location, r = row, displayBox = true, displayText = true, text = annotationText);
         }//Display gene.
         else if (typeGet(d) == 'exon') {
             var annotationText = `${typeGet(d)}`;
-            display(g=featureDisplay, type='exon', location, r=row, displayBox='true',displayText='true', text=annotationText);
+            display(type = 'exon', location, r = row, displayBox = true, displayText = true, text = annotationText);
         }//Display gene.
         else if (typeGet(d) == 'intron') {
             var annotationText = `${typeGet(d)}`;
-            display(g=featureDisplay, type='intron', location, r=row, displayBox='true',displayText='true', text=annotationText);
+            display(type = 'intron', location, r = row, displayBox = true, displayText = true, text = annotationText);
         }//Display gene
         else if (typeGet(d) == 'misc_feature') {
             var annotationText = `${informationGet(d).note}`;
-            display(g=featureDisplay, type='misc_feature', location, r=row, displayBox='true',displayText='true', text=annotationText);
+            display(type = 'misc_feature', location, r = row, displayBox = true, displayText = true, text = annotationText);
         }//Display gene.
         else if (typeGet(d) == 'STS') {
-            var annotationText = `${informationGet(d).standard_name}(STS)`;
-            display(g=featureDisplay, type='misc_feature', location, r=row, displayBox='true',displayText='true', text=annotationText);
+            var annotationText = `STS ${informationGet(d).standard_name}`;
+            display(type = 'misc_feature', location, r = row, displayBox = true, displayText = true, text = annotationText);
         }//Display gene.
-        else if (typeGet(d) == 'variation'){
-            display(g=featureDisplay, type='variation', location, r=row, displayBox='true',displayText='false');
+        else if (typeGet(d) == 'variation') {
+            var annotationText = `${typeGet(d)}`;
+            display(type = 'variation', location, r = row, displayBox = true, displayText = false);
             i++;
         }
-        else if (typeGet(d) == 'gap'){
-            display(g=featureDisplay, type='gap', location, r=0, displayBox='true',displayText='false');
+        else if (typeGet(d) == 'gap') {
+            var annotationText = `${typeGet(d)}`;
+            display(type = 'gap', location, r = 0, displayBox = true, displayText = true, text = annotationText);
             i++;
         }
         else {
             var annotationText = `${typeGet(d)}`;
-            display(g=featureDisplay, type='others', location, r=row, displayBox='true',displayText='true', text=annotationText);
+            display(type = 'others', location, r = row, displayBox = true, displayText = true, text = annotationText);
+            i++;
         }
     });
     beautify(featureArea);
-    //console.log(occupied[2]);
+
 }
 
 
 
-function trimText(element, i) {
-    var BBox = document.querySelector(`#${"feature-box"}${i}`);
+function trimText(i) {
+    var BBox = document.querySelector(`#feature-box-group-${i}`);
     const maxWidth = BBox.getBBox().width;
-    var annotation = document.querySelector(`${'#' + "feature-annotation" + i}`);
-    const textWidth = annotation.getBBox().width;
 
+    var annotation = document.querySelector(`#feature-annotation-${i}`);
+    const textWidth = annotation.getBBox().width;
+    //console.log(`maxWidth: ${maxWidth}, textWidth: ${maxWidth}`);
     if (textWidth > maxWidth) {
         text = annotation.textContent;
         fontSize = dms.fontSize;
         maxChar = maxWidth / fontSize;
         text = text.substring(0, maxChar);
         //console.log(maxChar);
-        text = text + "...";
-        //console.log(text);
-        element.select(`${'#' + "feature-annotation" + i}`)
-            .text(text);
+        text = text + "..";
+        console.log(text);
     }
+    return text
 } //Trim texts that are too long.
 
 function beautify(element) {
     element.selectAll('.box')
         .style('stroke-linecap', 'butt')
         .style('stroke-width', dms.boxHeight);
-    
+
     element.selectAll('.bone')
-        .style('opacity',0.5)
+        .style('opacity', 0.5)
         .style('stroke-linecap', 'butt')
+        .style('stroke-width', dms.boxHeight / 2)
         .style('stroke-dasharray', '3, 5');
 
     element.selectAll('.annotation')
         .style('font-size', dms.fontSize);
+
+    element.selectAll('.gap')
+        .style('stroke-linecap', 'butt')
+        .style('stroke-width', dms.boxHeight / 2);
+
+    element.selectAll('.variation')
+        .style('stroke-linecap', 'round')
+        .style('stroke-width', dms.boxHeight/5);
+        
 }
 
 function compareNumbers(a, b) {
     return a - b;
-  }
+}
 
-function checkOccupation(row, interval, occupied){
+function checkOccupation(row, interval, occupied) {
     //interval.sort(compareNumbers);
     console.log(`row:${row}`);
     intervalStart = interval[0];
@@ -263,32 +331,3 @@ function checkOccupation(row, interval, occupied){
     }
     return false;
 }
-
-/*
-featureArea.selectAll('line')
-    .data(featureGet(dataset))
-    .join('line')
-    .attr('x1',d=>LengthScale(Number(locationGet(d)[0])))
-    .attr('y1',0)
-    .attr('x2',d=>LengthScale(Number(locationGet(d)[1])))
-    .attr('y2',0)
-    .style('transform',`translateY(${dms.contentHeight/2 * Math.random()}px)`);
-*/
-
-
-
-/*
-设定画图板尺寸  dms
-设定组件 .append()
-
-导入数据 .json().then(); xGet
-
-设定坐标 .scaleLinear()
-
-画画 .selectAll().data().join()
-
-标注
-
-交互
-
-*/
